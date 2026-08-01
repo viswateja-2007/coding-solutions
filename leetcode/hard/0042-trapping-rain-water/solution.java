d9c829f464457db654b6f9d158737c8cde457c5f
Viswa Teja <viswateja6140@gmail.com>
2026-08-01 19:42:31 +0530

    ✅ Trapping Rain Water | Hard | Java | Runtime: 1 ms (59.32%) | Memory: 48.7 MB | 2026-08-01

--- leetcode/hard/0042-trapping-rain-water/solution.java
+++ leetcode/hard/0042-trapping-rain-water/solution.java
@@ -1,47 +1,41 @@
 class Solution {
     public int trap(int[] height) {
-        int min = 0;
-        int trappedWater = 0;
-        int width = 1;
 
+        int vol = 0;
         int n = height.length;
-
         int left = height[0];
         int right = height[n - 1];
 
-        int leftmax[] = new int[n];
-        int rightmax[] = new int[n];
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
 
         for(int i = 0; i < n; i++){
-            if(height[i] >= left){
+            if(left <= height[i]){
                 left = height[i];
-                leftmax[i] = height[i];
+                leftMax[i] = height[i];
             }
 
             else{
-                leftmax[i] = left;
+                leftMax[i] = left;
             }
         }
 
         for(int i = n - 1; i >= 0; i--){
-            if(height[i] >= right){
+            if(right <= height[i]){
                 right = height[i];
-                rightmax[i] = height[i];
+                rightMax[i] = height[i];
             }
 
             else{
-                rightmax[i] = right;
+                rightMax[i] = right;
             }
         }
 
-
         for(int i = 0; i < n; i++){
-            min = Math.min(leftmax[i], rightmax[i]);
-            int water = (min - height[i])*width;
-            trappedWater += water;
+            vol += (Math.min(rightMax[i], leftMax[i]) - height[i]);
         }
 
-        return trappedWater;
+        return vol;
 
         
     }
