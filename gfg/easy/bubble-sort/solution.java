afe7fdfac69c4f2aabe648fc7fdbb9987f8230ab
Viswa Teja <viswateja6140@gmail.com>
2026-08-02 20:01:54 +0530

    ✅ Bubble Sort | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-02

--- gfg/easy/bubble-sort/solution.java
+++ gfg/easy/bubble-sort/solution.java
@@ -1,5 +1,16 @@
 class Solution {
     public void bubbleSort(int[] arr) {
-        Arrays.sort(arr);
+        int n = arr.length;
+        int temp = 0;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - 1 - i; j++){
+                if(arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
     }
 }
