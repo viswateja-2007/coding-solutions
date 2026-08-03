de18cfe34b9ce886122e85feefef86bfa72ab2aa
Viswa Teja <viswateja6140@gmail.com>
2026-08-03 12:12:02 +0530

    ✅ Binary Search | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 48.5 MB | 2026-08-03

--- leetcode/easy/0704-binary-search/solution.java
+++ leetcode/easy/0704-binary-search/solution.java
@@ -6,7 +6,7 @@ class Solution {
 
         while(start <= end){
 
-            int mid = end - (end - start)/2;
+            int mid = start + (end - start)/2;
 
             if(nums[mid] == target){
                 return mid;
