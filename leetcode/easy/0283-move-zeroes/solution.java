33bc7d0c6ba6328f862e4c5567441369fbe80f6b
Viswa Teja <viswateja6140@gmail.com>
2026-08-21 23:29:06 +0530

    ✅ Move Zeroes | Easy | Java | Runtime: 2 ms (91.97%) | Memory: 47.8 MB | 2026-08-21

---
+++ leetcode/easy/0283-move-zeroes/solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int k = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
+                k++;
+            }
+        }
+
+    }
+}
