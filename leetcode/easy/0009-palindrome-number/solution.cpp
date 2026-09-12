4da220d78beb12f9496b7527ca3429c0fc5c4907
Viswa Teja <viswateja6140@gmail.com>
2026-09-12 09:57:28 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 3 ms (43.83%) | Memory: 8.6 MB | 2026-09-12

--- leetcode/easy/0009-palindrome-number/solution.cpp
+++ leetcode/easy/0009-palindrome-number/solution.cpp
@@ -3,18 +3,18 @@ public:
     bool isPalindrome(int x) {
         int temp = x;
         long long num = 0;
+
+        if(x < 0){
+            return false;
+        }
+
         while(x > 0){
             int lastDigit = x % 10;
             num = num * 10 + lastDigit;
             x = x / 10;
         }
 
-        if(num == temp){
-            return true;
-        }
+        return temp == num;
 
-        else{
-            return false;
-        }
     }
 };
